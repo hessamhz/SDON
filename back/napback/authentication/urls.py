845b4c87691a48a7d61a190715cda46a181febf0
Fatih Temiz <tmzfatih@gmail.com>
2024-02-03 19:02:10 +0100

    User Dashboard Added

--- back/napback/authentication/urls.py
+++ back/napback/authentication/urls.py
@@ -1,4 +1,5 @@
 from authentication.views import login_user, dashboard
+from django.contrib.auth.views import LogoutView
 from django.contrib import admin
 from django.urls import path
 
@@ -6,4 +7,6 @@ urlpatterns = [
     # for simplicity we use the base path
     path("", login_user, name="login"),
     path("dashboard/", dashboard, name="dashboard"),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+
 ]
--- back/napback/authentication/views/login.py
+++ back/napback/authentication/views/login.py
@@ -1,33 +1,25 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
-from django.shortcuts import HttpResponse, redirect, render
-
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.contrib import messages
 
 def login_user(request):
-    # Checks if form is submitted
     if request.method == "POST":
-        #data = {
-        #    "username": request.POST.get("username"),
-        #    "password": request.POST.get("password"),
-        #}
-        form = AuthenticationForm(data=request.POST)
-        print(form)
+        form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             username = form.cleaned_data.get("username")
             password = form.cleaned_data.get("password")
-            user = authenticate(request, username=username, password=password)
+            user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return HttpResponse("user logged in")
+                return redirect(reverse('dashboard'))
             else:
-                return HttpResponse("user doesn't exist")
+                messages.error(request, "Username or password is incorrect.")
         else:
-            # Render the counter.html template with the invalid form
+            messages.error(request, "Invalid form submission.")
+            # Render the index.html template with the invalid form
             return render(request, "hpanel/index.html", {"form": form})
     else:
-        # Create a new form instance for GET requests
         form = AuthenticationForm()
-
-    # Render the counter.html template with the form assuming that it is a GET
-    # return render(request, "counter.html", {"form": form})
     return render(request, "hpanel/index.html", {"form": form})
