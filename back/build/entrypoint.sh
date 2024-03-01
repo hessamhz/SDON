f861680685be27b40d88280a40df2fe513c6caf1
Hessam Hashemizadeh <hessam.hashemi21@gmail.com>
2024-03-01 10:08:36 +0100

    feat: added the entrypoint for db ready

--- back/build/entrypoint.sh
+++ back/build/entrypoint.sh
@@ -1,4 +1,33 @@
 #!/bin/sh
+set -o errexit
+
+set -o pipefail
+
+set -o nounset
+
+postgres_ready() {
+python << END
+import sys
+import psycopg2
+try:
+    psycopg2.connect(
+        dbname="${DEFAULT_DATABASE_NAME}",
+        user="${DEFAULT_DATABASE_USER}",
+        password="${DEFAULT_DATABASE_PASSWORD}",
+        host="${DEFAULT_DATABASE_HOST}",
+        port="${DEFAULT_DATABASE_PORT}",
+    )
+except psycopg2.OperationalError:
+    sys.exit(-1)
+sys.exit(0)
+END
+}
+
+until postgres_ready; do
+ >&2 echo "Waiting for PostgreSQL to become available....:-("
+ sleep 1
+done
+>&2 echo "PostgreSQL is ready!!!!...:-)"
 
 python manage.py migrate
 python manage.py collectstatic --noinput
